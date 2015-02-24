91b410c34d34444ecb58db070bdba53c2b817155
sagacitysite <carlo.michaelis@gmail.com>
2015-02-24 18:56:28 +0000

    topic design

--- public/js/app/views/topics/details.js
+++ public/js/app/views/topics/details.js
@@ -6,13 +6,45 @@ define([
     Template
     ) {
         
+    var ht = 0;
+        
     var View = Marionette.ItemView.extend({
         template: Template,
 
-        // events: {
-        //     'click .del': function(e) {
-        //         this.model.destroy();
-        //     },
+        events: {
+            'click .open-desc': function(e) {
+                //alert(ht);
+                $('.desc').animate({height: ht + 'px'}, 500 );
+                $('.open-desc').slideUp(250);
+            },
+            'click .del': function(e) {
+                if (confirm('Do you really want to delete the topic "'+this.model.get('name')+'"?')) {
+                    this.model.destroy();
+                    window.location.href='/#/topics';
+                }
+            },
+            'click .vote': function(e) {
+                e.preventDefault();
+                
+                if(this.model.get('voted')) {
+                    // if we have already voted then unvote
+                    $.post('/json/topic-unvote',
+                           {'tid':this.model.get('_id')},
+                           function(data,status) {
+                               this.model.set('votes',data);
+                               this.model.set('voted',0);
+                               this.render();
+                           }.bind(this));
+                } else {
+                    $.post('/json/topic-vote',
+                           {'tid':this.model.get('_id')},
+                           function(data,status) {
+                               this.model.set('votes',data);
+                               this.model.set('voted',1);
+                               this.render();
+                           }.bind(this));
+                }
+            }
         //     'click .voteup': function(e) {
         //         $.post('/json/topic-vote',
         //               {'tid':this.model.get('_id')},
@@ -31,12 +63,28 @@ define([
                 
         //         $(".lightbox").fadeIn(500);
         //     }
-        // },
+        },
         
         initialize: function() {
             var that = this;
             var fetching = this.model.fetch();
             fetching.done(function() {that.render();});
+            
+            Handlebars.registerHelper('ifis', function(a, b, opts) {
+                if(a == b) {
+                    return opts.fn(this);
+                } else {
+                    return opts.inverse(this);
+                }
+            });
+        },
+        
+        onRender: function() {
+            ht = $('.desc').height();
+            if(ht > 300) {
+                $('.desc').height(200);
+                $(".open-desc").css("display", "block");
+            }
         }
     });
     
